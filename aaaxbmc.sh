29155365de536b57d3133c654ffe1e647a11e897
Roy Chen <roy75117.mobile@gmail.com>
2014-05-28 11:53:59 +0900

    update method
    1.mount tmp on tmpfs(RAM)
    2.start at rc.local
    3.stop at halt or reboot

--- aaaxbmc.sh
+++ aaaxbmc.sh
@@ -3,19 +3,21 @@ sudo apt-get -y install rsync
 sudo service xbmc stop
 sudo nano /etc/init.d/aaxbmc
 #-------------------
-#  create shell script
+# create aaxbmc shell script
 #-------------------
 sudo chmod +x /etc/init.d/aaxbmc
-cd /etc/rcS.d
-sudo ln -s ../init.d/aaxbmc S50aaxbmc
-cd /etc/rc1.d
-sudo ln -s ../init.d/aaxbmc K10aaxbmc
+#cd /etc/rcS.d
+#sudo ln -s ../init.d/aaxbmc S50aaxbmc
+#cd /etc/rc1.d
+#sudo ln -s ../init.d/aaxbmc K10aaxbmc
 cd /etc/rc0.d
-sudo ln -s ../init.d/aaxbmc K01aaxbmc
+sudo ln -s ../init.d/aaxbmc K03aaxbmc
+cd /etc/rc6.d
+sudo ln -s ../init.d/aaxbmc K03aaxbmc
 
-cp -r /home/pi/.xbmc /run/shm
+cp -r /home/pi/.xbmc /tmp
 #sudo ln -s /run/shm/.xbmc /home/pi/.xbmc
-sudo mount --bind /run/shm/.xbmc /home/pi/.xbmc
+sudo mount --bind /tmp/.xbmc /home/pi/.xbmc
 #sudo mount -o bind /run/shm/.xbmc /home/pi/.xbmc
 mkdir /home/pi/.xbmc-backup
 sudo service xbmc start
@@ -28,13 +30,15 @@ sudo service xbmc start
 
 case "$1" in
   start)
-rsync -aog --delete-after --delay-updates /home/pi/.xbmc-backup/.xbmc /run/shm/ &> /dev/null
-sudo mount --bind /run/shm/.xbmc /home/pi/.xbmc
-#sudo mount -o bind /run/shm/.xbmc /home/pi/.xbmc
+	sudo service xbmc stop
+	rsync -aog --delete-after --delay-updates /home/pi/.xbmc-backup/.xbmc /tmp/ &> /dev/null
+	sudo mount --bind /tmp/.xbmc /home/pi/.xbmc
+	sudo service xbmc start
+	#sudo mount -o bind /run/shm/.xbmc /home/pi/.xbmc
     ;;
   stop)
-rsync -aog --delete-after --delay-updates /run/shm/.xbmc /home/pi/.xbmc-backup &> /dev/null
-sync
+	rsync -aog --delete-after --delay-updates /tmp/.xbmc /home/pi/.xbmc-backup &> /dev/null
+	sync
     ;;
   *)
     echo "Usage: /etc/init.d/aaxbmc {start|stop}"
@@ -44,3 +48,16 @@ esac
 
 exit 0
 #-------------------------------------------------------------#
+
+#---------------------/etc/rc.local---------------------------#
+/etc/init.d/aaxbmc start
+#-------------------------------------------------------------#
+
+#---------------------/etc/fstab------------------------------#
+proc            /proc           proc    defaults         0       0
+devpts          /dev/pts        devpts  rw,nosuid,noexec,relatime,gid=5,mode=620        0       0
+/dev/mmcblk0p1  /boot           vfat    defaults         0       0
+/dev/mmcblk0p2  /               ext4    defaults,noatime 0       0
+tmpfs           /tmp            tmpfs   defaults,noatime,mode=1777        0       0
+#--------------------------------------------------------------#
+
