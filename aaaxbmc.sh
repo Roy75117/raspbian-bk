47bdf34c8407386a8974bd00a2521249dfef8b90
Roy Chen <roy75117.mobile@gmail.com>
2014-06-20 06:13:21 +0900

    Update aaaxbmc.sh

--- aaaxbmc.sh
+++ aaaxbmc.sh
@@ -50,7 +50,9 @@ exit 0
 
 #----/etc/init/mountall.conf------#
 if [ $(mount | grep "tmpfs on /tmp" | wc -l) -gt 0 ];then
-  /etc/init.d/aaxbmc start
+   if [ $(mount | grep "home/pi/.xbmc type none (rw,bind)" | wc -l) -lt 1 ]$
+      /etc/init.d/aaxbmc start
+   fi
 fi
 #-------------------------------------------------------------#
 
