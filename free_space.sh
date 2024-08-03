2cd1567d09e1a74c79276a63deebc93624082286
Roy Chen <roy75117.mobile@gmail.com>
2024-08-03 17:53:17 +0900

    Update free_space.sh

--- free_space.sh
+++ free_space.sh
@@ -8,9 +8,9 @@ snap list --all | awk '/disabled/{print $1, $3}' |
     done
 
 # CLEAN linux-image pkg
-dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
-apt-get autoremove
-apt-get update
+#dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+#apt-get autoremove
+#apt-get update
 
 # CLEAN journal logs (default 3days)
 journalctl --vacuum-time=3d
